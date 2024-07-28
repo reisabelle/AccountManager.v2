e148ee920c3a798c1de542e5cf97b7fbaaeccc64
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-28 22:22:58 +0800

    Update info.kt

--- app/src/main/java/com/example/accountmanager/info.kt
+++ app/src/main/java/com/example/accountmanager/info.kt
@@ -1,27 +1,60 @@
 package com.example.accountmanager
 
+import Adapter
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class info : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var adapter: Adapter
+    private val database = FirebaseDatabase.getInstance()
+    private val accountsRef = database.getReference("Accounts")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val originalPassword = intent.getStringExtra("PASSWORD_KEY") //get the password value from panel
+        val originalEmail = intent.getStringExtra("EMAIL_KEY")
+
         val infobtn: ImageView = findViewById(R.id.backbtn5)
         infobtn.setOnClickListener () {
             val intent = Intent(this, panel::class.java)
+            intent.putExtra("PASSWORD_KEY", originalPassword)
+            intent.putExtra("EMAIL_KEY", originalEmail)
             startActivity(intent)
         }
+        listView = findViewById(R.id.LV_info)
+        adapter = Adapter(this, mutableListOf())
+        listView.adapter = adapter
+
+        fetchAccountsData()
+    }
+    private fun fetchAccountsData() {
+        accountsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val accounts = mutableListOf<Account_Model>()
+                for (accountSnapshot in snapshot.children) {
+                    val account = accountSnapshot.getValue(Account_Model::class.java)
+                    account?.let { accounts.add(it) }
+                }
+                adapter.updateData(accounts)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle the error here
+            }
+        })
     }
 }
