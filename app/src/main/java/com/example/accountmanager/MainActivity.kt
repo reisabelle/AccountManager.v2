36a9295117269b8fcf124c447de8dd26af4868f1
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-28 22:29:22 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/accountmanager/MainActivity.kt
+++ app/src/main/java/com/example/accountmanager/MainActivity.kt
@@ -10,18 +10,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val regbtn: Button = findViewById(R.id.regbtn)
         regbtn.setOnClickListener(){
