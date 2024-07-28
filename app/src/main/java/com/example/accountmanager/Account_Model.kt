4a36b6c961dfe5b3ef42a15214699a5c03250cf0
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-28 22:12:57 +0800

    Create Account_Model.kt
    added a model

---
+++ app/src/main/java/com/example/accountmanager/Account_Model.kt
@@ -0,0 +1,10 @@
+package com.example.accountmanager
+
+class Account_Model(
+    val platform: String = "",
+    val link: String = "",
+    val email: String = "",
+    val phoneNo: String = "",
+    val password: String = "",
+    val userEmail: String = ""
+) 
