c5469eed047dc9bb59a56ca65a3d37125f5e89ae
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-28 22:22:30 +0800

    Update home.kt

--- app/src/main/java/com/example/accountmanager/home.kt
+++ app/src/main/java/com/example/accountmanager/home.kt
@@ -11,23 +11,28 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class home : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val originalPassword = intent.getStringExtra("PASSWORD_KEY")
+        val originalEmail = intent.getStringExtra("EMAIL_KEY")
+
         val accbtn: Button = findViewById(R.id.accbtn)
         accbtn.setOnClickListener() {
             val intent = Intent(this, Accounts::class.java)
+            intent.putExtra("PASSWORD_KEY", originalPassword)
+            intent.putExtra("EMAIL_KEY", originalEmail)
             startActivity(intent)
         }
+
         val menu: ImageView = findViewById(R.id.menu)
         menu.setOnClickListener() {
             val intent = Intent(this, panel::class.java)
+            intent.putExtra("PASSWORD_KEY", originalPassword)
+            intent.putExtra("EMAIL_KEY", originalEmail)
             startActivity(intent)
         }
     }
