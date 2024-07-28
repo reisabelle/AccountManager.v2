6b4b1771e82ec89b650a567082ccb2d7dba266c8
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-28 22:23:52 +0800

    Update panel.kt

--- app/src/main/java/com/example/accountmanager/panel.kt
+++ app/src/main/java/com/example/accountmanager/panel.kt
@@ -13,25 +13,29 @@ import com.example.accountmanager.home
 import com.example.accountmanager.passConfirmation
 
 class panel : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_panel)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val originalPassword = intent.getStringExtra("PASSWORD_KEY")//get the password value from home
+        val originalEmail = intent.getStringExtra("EMAIL_KEY")
+
         val button1: Button = findViewById(R.id.homebtn)
         button1.setBackgroundColor(Color.TRANSPARENT)
         button1.setOnClickListener{
             val intent = Intent(this, home::class.java)
+            intent.putExtra("PASSWORD_KEY", originalPassword) //pass the password value
+            intent.putExtra("EMAIL_KEY", originalEmail)
             startActivity(intent)
         }
         val button2: Button = findViewById(R.id.infobtn)
         button2.setBackgroundColor(Color.TRANSPARENT)
+
         button2.setOnClickListener{
             val intent = Intent(this, passConfirmation::class.java)
+            intent.putExtra("PASSWORD_KEY", originalPassword) //pass the password value
+            intent.putExtra("EMAIL_KEY", originalEmail)
             startActivity(intent)
         }
         val button3: Button = findViewById(R.id.logoutbtn)
