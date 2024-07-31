1d953ea673396f26d3460fd527d9ed1f0311cf50
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-31 17:28:56 +0800

    Update registeredUser.kt

--- app/src/main/java/com/example/accountmanager/registeredUser.kt
+++ app/src/main/java/com/example/accountmanager/registeredUser.kt
@@ -2,14 +2,23 @@ package com.example.accountmanager
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.accountmanager.ModelClassess.Users
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class registeredUser : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var adapter: user_Adapter
+    private val database = FirebaseDatabase.getInstance()
+    private val accountsRef = database.getReference("users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registered_user)
@@ -19,5 +28,29 @@ class registeredUser : AppCompatActivity() {
             val intent = Intent(this, Admin::class.java)
             startActivity(intent)
         }
+
+        listView = findViewById(R.id.LV_users)
+        adapter = user_Adapter(this, mutableListOf())
+        listView.adapter = adapter
+
+        fetchAccountsData()
+    }
+
+    private fun fetchAccountsData() {
+        accountsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val users = mutableListOf<Users>()
+                for (accountSnapshot in snapshot.children) {
+                    val account = accountSnapshot.getValue(Users::class.java)
+                    account?.let { users.add(it) }
+                }
+                Log.d("FetchAccountsData", "Users fetched: ${users.size}")
+                adapter.updateData(users)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("FetchAccountsData", "Error fetching data", error.toException())
+            }
+        })
     }
 }
