0815d3bffc92cbc0d43f6299bcf0a9d8601339c4
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-31 17:27:25 +0800

    Update Users.kt

--- app/src/main/java/com/example/accountmanager/ModelClassess/Users.kt
+++ app/src/main/java/com/example/accountmanager/ModelClassess/Users.kt
@@ -1,50 +1,8 @@
 package com.example.accountmanager.ModelClassess
 
-class Users {
-    private var email: String = ""
-    private var imageUrl: String = ""
-    private var phone: String = ""
-    private var username: String = ""
-
-
-    constructor()
-
-    constructor(email: String, imageUrl: String, phone: String, username: String) {
-        this.email = email
-        this.imageUrl = imageUrl
-        this.phone = phone
-        this.username = username
-    }
-
-    fun getName(): String?{
-        return username
-    }
-
-    fun setName(username: String){
-        this.username = username
-    }
-
-    fun getEmail(): String?{
-        return email
-    }
-
-    fun setEmail(email: String){
-        this.email = email
-    }
-
-    fun getImgUrl(): String?{
-        return imageUrl
-    }
-
-    fun setImgUrl(imageUrl: String){
-        this.imageUrl = imageUrl
-    }
-
-    fun getPhone(): String?{
-        return phone
-    }
-
-    fun setPhone(imageUrl: String){
-        this.phone = phone
-    }
-}
+class Users(
+    val email: String = "",
+    val imageUrl: String = "",
+    val phone: String = "",
+    val username: String = ""
+)
