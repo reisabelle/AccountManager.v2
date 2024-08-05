1292508c890a7ed93e6f3df6d40ab6f499f0b719
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-08-06 00:18:52 +0800

    Update academic_records.kt

--- app/src/main/java/com/example/accountmanager/academic_records.kt
+++ app/src/main/java/com/example/accountmanager/academic_records.kt
@@ -1,12 +1,18 @@
 package com.example.accountmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class academic_records : AppCompatActivity() {
+    
+    val originalPassword = intent.getStringExtra("PASSWORD_KEY")
+    val originalEmail = intent.getStringExtra("EMAIL_KEY")
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_academic_records)
@@ -14,12 +20,16 @@ class academic_records : AppCompatActivity() {
         val back = findViewById<ImageView>(R.id.backbtn)
         back.setOnClickListener{
             val intent = Intent(this, home::class.java)
+            intent.putExtra("PASSWORD_KEY", originalPassword)
+            intent.putExtra("EMAIL_KEY", originalEmail)
             startActivity(intent)
         }
-        
+
         val add = findViewById<ImageView>(R.id.addbtn)
         add.setOnClickListener{
             val intent = Intent(this, add_record::class.java)
+            intent.putExtra("PASSWORD_KEY", originalPassword)
+            intent.putExtra("EMAIL_KEY", originalEmail)
             startActivity(intent)
         }
     }
