f955dab36352ce5ced1a97fc9518d5e561252891
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-08-06 13:30:12 +0800

    Create Records_Model.kt

---
+++ app/src/main/java/com/example/accountmanager/ModelClassess/Records_Model.kt
@@ -0,0 +1,9 @@
+package com.example.accountmanager.ModelClassess
+
+class Records_Model(
+    val semester: String = "",
+    val subject: String = "",
+    val grade: String = "",
+    val instructor: String = "",
+    val userEmail: String = ""
+)
